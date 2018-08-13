bdba0bae72203047fa23c05371f6b6c436f8efcc
mattperkins <github@mattperkins.com>
2018-08-13 09:21:57 +0100

    write to file

--- os.js
+++ os.js
@@ -1,7 +1,11 @@
 const os = require('os')
+const fs = require('fs')
 
 let user = os.userInfo()
-console.log(user)
-
 let platform = os.platform()
-console.log(platform)
+let date = new Date()
+let msg = `User: ${user.username} started app at ${date} on the '${platform}' platform`
+
+fs.appendFile("newFile.txt", msg, (err) => {
+ console.log('error') 
+})
